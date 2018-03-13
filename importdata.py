bc14bb33bfeaa34e22081eb760b5731980627277
shmakn99 <32480168+shmakn99@users.noreply.github.com>
2018-03-13 21:18:35 +0530

    First Commit

---
+++ importdata.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 13 12:15:03 2018
+
+@author: Manik Sharma
+"""
+
+with open('93062.protein.links.detailed.v10.5.txt') as f:
+    print (f.readline())
